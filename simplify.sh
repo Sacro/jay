f17a6a937afcc553d204e4cf74bb869041d57b29
Will Deakin <will.deakin@crinstitute.org.uk>
2023-04-20 19:46:06 +0100

    archive old output

--- simplify.sh
+++ simplify.sh
@@ -16,12 +16,17 @@ if [ ! -d ./node ]; then
     . ./node.sh
 fi
 
-for i in output
+for i in output archive
 do
     if [ ! -d ${i} ]; then
         mkdir -p ${i}
     fi
 done
+
+FILESTUB=$(basename ${FILEPATH} | sed 's/\..*$//')
+if [ -s output/${FILESTUB}-simple.gpkg ]; then
+    mv output/${FILESTUB}-simple.gpkg archive
+fi
 
 for layer in $(./simplifypoints.py ${FILEPATH})
 do
