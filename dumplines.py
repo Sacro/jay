03ef55cac879f56cf3b040de781b64b1c6bda78f
Will Deakin <will.deakin@crinstitute.org.uk>
2023-05-08 16:14:57 +0100

    more graceful fail on reading GeoJSON

--- dumplines.py
+++ dumplines.py
@@ -4,6 +4,7 @@ import sys
 import os
 os.environ['USE_PYGEOS'] = '0'
 import geopandas as gp
+from pyogrio.errors import DataSourceError
 from pyogrio import read_dataframe, write_dataframe, list_layers, read_info
 
 CRS = "EPSG:32630"
@@ -12,6 +13,11 @@ args = sys.argv[:]
 args.pop(0)
 
 FILEPATH, OUTPATH, LAYER = args
+FILEPATH, OUTPATH, LAYER = '/home/diablo/tmp/geography-simple.geojson', 'x.gpkg', 'outer'
+
 print(f'Update {FILEPATH} {OUTPATH} {LAYER}')
-GF = read_dataframe(FILEPATH).set_crs(CRS, allow_override=True)
-write_dataframe(GF, OUTPATH, layer=LAYER)
+try:
+    GF = read_dataframe(FILEPATH).set_crs(CRS, allow_override=True)    
+    write_dataframe(GF, OUTPATH, layer=LAYER)
+except DataSourceError:
+    print(f'Failed {FILEPATH} {OUTPATH} {LAYER}')
