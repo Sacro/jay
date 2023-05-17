f5cd5ae303a82928952d205c0e415a8da4f4cf89
Ben Woodward <ben@benwoodward.me.uk>
2023-05-17 21:12:51 +0100

    Remove anything trying to install things

--- maplayer.sh
+++ maplayer.sh
@@ -6,18 +6,8 @@ LAYER=$2
 
 echo ${FILESTUB} ${LAYER}
 
-NODE_PATH=./$(ls -d nvm/versions/node/*/bin | tail -1)
-export PATH=${PATH}:${NODE_PATH}:${PWD}/node/node_modules/mapshaper/bin/
-
-for FILE in ${FILESTUB}.geojson ${FILESTUB}-simple.geojson
-do
-    if [ ! -s ${TEMP}/${FILE} ]; then
-        rm ${TEMP}/${FILE}
-    fi
-done
-
 ./dumpgeojson.py ${FILEPATH} ${TEMP}/${FILESTUB}.geojson ${LAYER}
 
-mapshaper-xl 16gb ${TEMP}/${FILESTUB}.geojson precision=0.1 -snap endpoints interval=0.1 -simplify interval=1.0 keep-shapes -o format=geojson geojson-type=FeatureCollection ${TEMP}/${FILESTUB}-simple.geojson
+npx --package mapshaper mapshaper-xl 16gb ${TEMP}/${FILESTUB}.geojson precision=0.1 -snap endpoints interval=0.1 -simplify interval=1.0 keep-shapes -o format=geojson geojson-type=FeatureCollection ${TEMP}/${FILESTUB}-simple.geojson
 
 ./dumplines.py ${TEMP}/${FILESTUB}-simple.geojson output/${FILESTUB}-simple.gpkg ${LAYER}
--- simplify.sh
+++ simplify.sh
@@ -2,20 +2,6 @@
 
 FILEPATH=$1
 
-if [ ! -d venv ]; then
-    echo Set up python3 virtual environment
-    python3 -m venv venv
-    source venv/bin/activate
-    pip3 install --upgrade pip
-    pip3 install -r requirements.txt
-else
-    source venv/bin/activate
-fi
-
-if [ ! -d ./node ]; then
-    . ./node.sh
-fi
-
 for i in output archive
 do
     if [ ! -d ${i} ]; then
